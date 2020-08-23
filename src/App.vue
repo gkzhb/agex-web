a5d425f2ad61eb62d240d48e9930216490968af3
zhb <zhb896579388@163.com>
2020-08-23 23:54:19 +0800

    Add login page

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,31 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
 
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list nav>
+        <v-list-item> </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
     <v-main>
-      <HelloWorld />
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
-
 export default {
   name: "App",
 
-  components: {
-    HelloWorld
-  },
+  components: {},
 
   data: () => ({
-    //
+    drawer: false,
+    navList: []
   })
 };
 </script>
---
+++ src/views/Login.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-container>
+    <h2>登录</h2>
+    <v-form ref="loginForm" v-model="loginForm">
+      <v-text-field
+        id="username"
+        v-model="loginForm.username"
+        prepend-icon="mdi-account"
+        name="Username"
+        label="用户名"
+        :rules="[username => !!username || '用户名为空']"
+      ></v-text-field>
+      <v-text-field
+        id="password"
+        v-model="loginForm.password"
+        :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+        :type="showPassword ? 'text' : 'password'"
+        prepend-icon="mdi-lock"
+        name="Password"
+        label="密码"
+        @click:append="showPassword = !showPassword"
+        :rules="[password => !!password || '密码为空']"
+        :error-messages="errorMsgs"
+        :error="error"
+      ></v-text-field>
+      <v-btn color="primary" large @click="clickLogin">登录</v-btn>
+    </v-form>
+  </v-container>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      loginForm: {
+        username: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+    clickLogin() {
+      if (this.$refs.loginForm.validate()) {
+        // TODO: login
+      }
+    }
+  }
+};
+</script>
