7c7474bb6e8dbf4c37959c45027b41f3bc71b5d0
zhb <zhb896579388@163.com>
2020-12-10 21:21:48 +0800

    Update time format

--- src/utils/others.js
+++ src/utils/others.js
@@ -1,6 +1,15 @@
 import dayjs from "../plugins/dayjs";
 
+const today = dayjs().startOf("day");
+
 export function fromNow(t) {
   const dt = dayjs(t);
+  const diff = today.diff(dt, "day");
+  if (diff > 5) {
+    return dt.format("YYYY-MM-DD HH:mm:ss");
+  }
+  if (diff > 1) {
+    return dt.format("dddd HH:mm:ss");
+  }
   return dt.fromNow() + " " + dt.format("HH:mm:ss");
 }
