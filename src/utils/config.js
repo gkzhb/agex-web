c09f5d9ed63e5f6dccbd2c5c09543db2477feefe
zhb <zhb896579388@163.com>
2021-09-18 13:17:07 +0800

    fix: update age url

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,2 @@
 export const BASE_URL = process.env.VUE_APP_BASE_API;
-export const AGE_DETAIL_URL = "https://www.agefans.net/detail/";
+export const AGE_DETAIL_URL = "https://www.agefans.cc/detail/";
